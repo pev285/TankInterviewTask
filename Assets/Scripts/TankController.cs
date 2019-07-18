d2d73c47849175a79edf609ef2d113298a53b072
MSI-PEV-GP72M\pev <prosolupov@inbox.ru>
2019-07-19 01:49:50 +0300

    Project created.

---
+++ Assets/Scripts/TankController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TanksInterviewDemo
+{
+	public class TankController : MonoBehaviour 
+	{
+        private float MovementSpeed;
+
+        private Rigidbody RB;
+        private Transform Transform;
+
+        private float VerticalAxis;
+        private float HorizontalAxis;
+
+        private float Mouse;
+
+        private void Awake()
+        {
+            RB = GetComponent<Rigidbody>();
+            Transform = GetComponent<Transform>();
+        }
+
+        private void Update()
+        {
+            VerticalAxis = Input.GetAxis("Vertical");
+            HorizontalAxis = Input.GetAxis("Horizontal");
+        }
+
+        private void FixedUpdate()
+        {
+            var velocity = new Vector3(HorizontalAxis, VerticalAxis, 0).normalized * MovementSpeed;
+
+            RB.velocity = velocity;
+        }
+
+    } // END OF CLASS ///
+	
+} // END OF NAMESPACE ///
+
+
